5f2bada08f724e503ebc30a9ae09b2a1803ff606
Suman <suman@gmail.com>
2025-01-14 12:32:59 +0530

    Add condition

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,10 @@ use App\Models\Company;
 class UserController extends Controller
 {
     public function index(){
-        $users = User::with('company')->get();
+        // $users = User::with('company')->get();
+        $users = User::with('company')->whereHas('company',function($query){
+            $query->where('name','like','%Yahoo%');
+        })->get();
         return $users;
     }
 
